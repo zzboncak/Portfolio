eafbf960d23da43c6e76c9fff182c6bcd606c848
Zachary <zachary.zboncak@gmail.com>
2019-10-07 19:17:56 -0500

    implemented feedback edits

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 function popup() {
     $('.parent').on('mouseenter', event => {
         let parent = $(event.currentTarget);
-        parent.children('h4').toggleClass('hidden');
-        parent.children('h4').toggleClass('popup');
+        parent.children('p').toggleClass('hidden');
+        parent.children('p').toggleClass('popup');
     })
 }
 
 function disappear() {
     $('.parent').on('mouseleave', event => {
         let parent = $(event.currentTarget);
-        parent.children('h4').toggleClass('hidden');
-        parent.children('h4').toggleClass('popup');
+        parent.children('p').toggleClass('hidden');
+        parent.children('p').toggleClass('popup');
     })
 }
 
